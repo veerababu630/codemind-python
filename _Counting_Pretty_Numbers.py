95cb8879de6a17123c4e1ce8b2b029984ab27e7d
veerababu630 <84891527+veerababu630@users.noreply.github.com>
2022-10-31 12:02:23 +0530

    t=int(input()) for i in range(t):     n,m=map(int,input().split())     c=0     for i in range(n,m+1):         r=i%10         if(r==2 or r==3 or r==9):             c+=1     print(c)

---
+++ _Counting_Pretty_Numbers.py
@@ -0,0 +1,9 @@
+t=int(input())
+for i in range(t):
+    n,m=map(int,input().split())
+    c=0
+    for i in range(n,m+1):
+        r=i%10
+        if(r==2 or r==3 or r==9):
+            c+=1
+    print(c)        
